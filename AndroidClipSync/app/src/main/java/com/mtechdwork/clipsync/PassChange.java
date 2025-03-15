ab2afe490f55439769ee6f5118d3e0411c74cb8a
M2Tech <maiquanghai011@gmail.com>
2025-03-15 19:32:29 +0700

    Add username

--- AndroidClipSync/app/src/main/java/com/mtechdwork/clipsync/PassChange.java
+++ AndroidClipSync/app/src/main/java/com/mtechdwork/clipsync/PassChange.java
@@ -2,7 +2,6 @@ package com.mtechdwork.clipsync;
 
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -14,7 +13,6 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.Objects;
 
 public class PassChange extends AppCompatActivity {
@@ -27,6 +25,7 @@ public class PassChange extends AppCompatActivity {
     private TextView edtOldPass;
     private TextView edtNewPass;
     private TextView edtNewPassConfirm;
+    private TextView edtUsername;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,22 +42,46 @@ public class PassChange extends AppCompatActivity {
 
         settingManager = new SettingManager(this);
 
+        String username = settingManager.getUsername();
+        if (!username.equals("FAILED_TO_GET_USERNAME")) edtUsername.setText(username);
+        else edtUsername.setText("");
+
         btnApply.setOnClickListener(v -> {
-            if (checkPasswordMatch(edtOldPass.getText().toString())) {
-                if (edtNewPass.getText().equals(edtNewPassConfirm.getText()))
-                    settingManager.setPassword(sha512(edtNewPass.getText().toString()));
-                else
-                    Toast.makeText(this, "Xác nhận mật khẩu mới không trùng khớp!", Toast.LENGTH_SHORT).show();
+            if (checkPasswordMatch(edtOldPass.getText().toString())) { // Check current password
+                if (writeUsername()) writePassword();
             } else
                 Toast.makeText(this, "Sai mật khẩu hiện tại!", Toast.LENGTH_SHORT).show();
         });
     }
 
-    private String sha512(String input) {
+    private boolean writeUsername() {
+        String username = edtUsername.getText().toString();
+        if (!username.equals("FAILED_TO_GET_USERNAME")) {
+            settingManager.setUsername(username); // Write username
+            return true;
+        }
+        Toast.makeText(this, "Tên người dùng không hợp lệ!", Toast.LENGTH_SHORT).show();
+        return false;
+    }
+
+    private void writePassword() {
+        String password = edtNewPass.getText().toString();
+        String username = settingManager.getUsername();
+        if (password.equals(edtNewPassConfirm.getText().toString())) {
+            if (!password.isEmpty())
+                settingManager.setPassword(sha512(username, password)); // Write password
+            Toast.makeText(this, "Thay đổi thông tin thành công!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        Toast.makeText(this, "Xác nhận mật khẩu mới không trùng khớp!", Toast.LENGTH_SHORT).show();
+    }
+
+    private String sha512(String password, String salt) {
         try {
             MessageDigest md = MessageDigest.getInstance("SHA-512");
 
-            byte[] digest = md.digest(input.getBytes());
+            String passWithSalt = salt + password;
+            byte[] digest = md.digest(passWithSalt.getBytes());
             StringBuilder sb = new StringBuilder();
             for (byte b : digest)
                 sb.append(Integer.toString((b & 0xff) + 0x100, 16).substring(1));
@@ -72,7 +95,8 @@ public class PassChange extends AppCompatActivity {
 
     private boolean checkPasswordMatch(String password) {
         String currentPass = settingManager.getPassword();
-        if (!password.isEmpty()) return sha512(password).equals(currentPass);
+        String username = settingManager.getUsername();
+        if (!password.isEmpty()) return sha512(username, password).equals(currentPass);
         return password.equals(currentPass);
     }
 
@@ -81,5 +105,6 @@ public class PassChange extends AppCompatActivity {
         edtOldPass = findViewById(R.id.edtOldPass);
         edtNewPass = findViewById(R.id.edtNewPass);
         edtNewPassConfirm = findViewById(R.id.edtNewPassConfirm);
+        edtUsername = findViewById(R.id.edtUsername);
     }
 }
--- AndroidClipSync/app/src/main/java/com/mtechdwork/clipsync/SettingManager.java
+++ AndroidClipSync/app/src/main/java/com/mtechdwork/clipsync/SettingManager.java
@@ -3,13 +3,11 @@ package com.mtechdwork.clipsync;
 import android.content.Context;
 import android.util.Log;
 
-import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.Objects;
 
 public class SettingManager extends FileHandler {
-    private boolean debug = true;
 
     SettingManager(Context context) {
         super(context);
@@ -18,6 +16,7 @@ public class SettingManager extends FileHandler {
                 JSONObject jsonObject = new JSONObject();
 
                 // Init json data
+                jsonObject.put("username", "");
                 jsonObject.put("enable_sync", false);
                 jsonObject.put("password", "");
 
@@ -25,8 +24,54 @@ public class SettingManager extends FileHandler {
                 writeData(jsonString);
             }
         } catch (Exception e) {
-            if (debug) Log.e("[Setting Manager]", Objects.requireNonNull(e.getMessage()));
+            debugInfo(Objects.requireNonNull(e.getMessage()), 2);
         }
+    }
+
+    private void debugInfo(String message, int type) {
+        // Type: 0 - Info, 1 - Warning, 2 - Error
+        boolean debug = true;
+        if (!debug) return;
+        String className = "[Setting Manager]";
+        switch (type) {
+            case 0:
+                Log.i(className, message);
+                break;
+
+            case 1:
+                Log.w(className, message);
+                break;
+
+            case 2:
+                Log.e(className, message);
+        }
+    }
+
+    public void setUsername(String username) {
+        try {
+            JSONObject jsonObject = new JSONObject(readData());
+
+            if (!username.equals("FAILED_TO_GET_USERNAME"))
+                jsonObject.put("username", username);
+            else {
+                jsonObject.put("username", "Username");
+            }
+
+            String jsonString = jsonObject.toString(4);
+            writeData(jsonString);
+        } catch (Exception e) {
+            debugInfo(Objects.requireNonNull(e.getMessage()), 2);
+        }
+    }
+
+    public String getUsername() {
+        try {
+            JSONObject jsonObject = new JSONObject(readData());
+            return jsonObject.getString("username");
+        } catch (Exception e) {
+            debugInfo(Objects.requireNonNull(e.getMessage()), 2);
+        }
+        return "FAILED_TO_GET_USERNAME";
     }
 
     public void setPassword(String password) {
@@ -38,7 +83,7 @@ public class SettingManager extends FileHandler {
             String jsonString = jsonObject.toString(4);
             writeData(jsonString);
         } catch (Exception e) {
-            if (debug) Log.e("[Setting Manager]", Objects.requireNonNull(e.getMessage()));
+            debugInfo(Objects.requireNonNull(e.getMessage()), 2);
         }
     }
 
@@ -47,7 +92,7 @@ public class SettingManager extends FileHandler {
             JSONObject jsonObject = new JSONObject(readData());
             return jsonObject.getString("password");
         } catch (Exception e) {
-            if (debug) Log.e("[Setting Manager]", Objects.requireNonNull(e.getMessage()));
+            debugInfo(Objects.requireNonNull(e.getMessage()), 2);
         }
         return "FAILED_TO_GET_PASS";
     }
@@ -61,7 +106,7 @@ public class SettingManager extends FileHandler {
             String jsonString = jsonObject.toString(4);
             writeData(jsonString);
         } catch (Exception e) {
-            if (debug) Log.e("[Setting Manager]", Objects.requireNonNull(e.getMessage()));
+            debugInfo(Objects.requireNonNull(e.getMessage()), 2);
         }
     }
 
@@ -70,7 +115,7 @@ public class SettingManager extends FileHandler {
             JSONObject jsonObject = new JSONObject(readData());
             return jsonObject.getBoolean("enable_sync");
         } catch (Exception e) {
-            if (debug) Log.e("[Setting Manager]", Objects.requireNonNull(e.getMessage()));
+            debugInfo(Objects.requireNonNull(e.getMessage()), 2);
         }
         return false;
     }
