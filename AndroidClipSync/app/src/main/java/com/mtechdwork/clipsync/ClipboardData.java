1105bbcc2420480bdd0f2461f77f376f405bb39a
M2Tech <maiquanghai011@gmail.com>
2025-03-19 00:22:08 +0700

    Add classes

---
+++ AndroidClipSync/app/src/main/java/com/mtechdwork/clipsync/ClipboardData.java
@@ -0,0 +1,48 @@
+package com.mtechdwork.clipsync;
+
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.util.Log;
+
+public class ClipboardData {
+    private static String LATEST_DATA;
+
+    public static String getLatestData() {
+        return LATEST_DATA;
+    }
+
+    public static void setLatestData(String latestData) {
+        LATEST_DATA = latestData;
+    }
+
+    private static void log(String message, int type) {
+        // Type: 0 - Info, 1 - Warning, 2 - Error
+        boolean debug = true;
+        if (!debug) return;
+        String className = "[Clipboard Data]";
+        switch (type) {
+            case 0:
+                Log.i(className, message);
+                break;
+
+            case 1:
+                Log.w(className, message);
+                break;
+
+            case 2:
+                Log.e(className, message);
+        }
+    }
+
+    public static void writeClipboard(Context context, String text) {
+        if (!text.isEmpty()) {
+            ClipboardManager clipboard = (ClipboardManager) context.getSystemService(Context.CLIPBOARD_SERVICE);
+            ClipData clip = ClipData.newPlainText("Copied Text", text);
+            clipboard.setPrimaryClip(clip);
+            setLatestData(text);
+
+            log("Writed to clipboard: " + text, 0);
+        }
+    }
+}
---
+++ AndroidClipSync/app/src/main/java/com/mtechdwork/clipsync/JsonGen.java
@@ -0,0 +1,55 @@
+package com.mtechdwork.clipsync;
+
+import android.util.Log;
+
+import org.json.JSONObject;
+
+public class JsonGen {
+    private static void log(String message, int type) {
+        // Type: 0 - Info, 1 - Warning, 2 - Error
+        boolean debug = true;
+        if (!debug) return;
+        String className = "[JsonGen]";
+        switch (type) {
+            case 0:
+                Log.i(className, message);
+                break;
+
+            case 1:
+                Log.w(className, message);
+                break;
+
+            case 2:
+                Log.e(className, message);
+        }
+    }
+    public static String otpAuthJson(String otp) {
+        try {
+            JSONObject jsonObject = new JSONObject();
+
+            jsonObject.put("type", "otp_auth");
+            jsonObject.put("value", otp);
+
+            return jsonObject.toString();
+        } catch (Exception e) {
+            log("EXCEPTION", 2);
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String dataJson(String data) {
+        try {
+            JSONObject jsonObject = new JSONObject();
+
+            jsonObject.put("type", "data");
+            jsonObject.put("value", data);
+
+            return jsonObject.toString();
+        } catch (Exception e) {
+            log("EXCEPTION", 2);
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
