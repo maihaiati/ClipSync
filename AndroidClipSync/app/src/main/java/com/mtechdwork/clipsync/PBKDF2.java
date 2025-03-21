f61f61dfe2b239ea9c010a2bffd32eb877c2691d
M2Tech <maiquanghai011@gmail.com>
2025-03-21 11:16:54 +0700

    Create PBKDF2.java

---
+++ AndroidClipSync/app/src/main/java/com/mtechdwork/clipsync/PBKDF2.java
@@ -0,0 +1,29 @@
+package com.mtechdwork.clipsync;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Looper;
+
+import java.security.SecureRandom;
+import java.security.spec.KeySpec;
+import java.util.Base64;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+
+public class PBKDF2 extends Thread {
+
+    private static final int ITERATIONS = 100_000;
+    private static final int KEY_LENGTH = 256; // 256-bit key
+    private static final String PBKDF2_ALGORITHM = "PBKDF2WithHmacSHA1";
+
+    public static byte[] deriveKey(Context context) throws Exception { // PBKDF2 chỉ dùng để dẫn xuất khoá ở đây (không dùng để chống tấn công)
+        SettingManager settingManager = new SettingManager(context);
+        String password = settingManager.getPassword();
+        String username = settingManager.getUsername();
+
+        KeySpec spec = new PBEKeySpec(password.toCharArray(), username.getBytes(), ITERATIONS, KEY_LENGTH);
+        SecretKeyFactory factory = SecretKeyFactory.getInstance(PBKDF2_ALGORITHM);
+        return factory.generateSecret(spec).getEncoded();
+    }
+}
