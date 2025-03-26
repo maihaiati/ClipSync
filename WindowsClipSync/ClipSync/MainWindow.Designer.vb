abe3b88c6a383d23218e4e0850bc984f0d6c9d03
M2Tech <maiquanghai011@gmail.com>
2025-03-26 21:28:37 +0700

    Update GUI

--- WindowsClipSync/ClipSync/MainWindow.Designer.vb
+++ WindowsClipSync/ClipSync/MainWindow.Designer.vb
@@ -22,43 +22,52 @@ Partial Class MainWindow
 	'Do not modify it using the code editor.
 	<System.Diagnostics.DebuggerStepThrough()>
 	Private Sub InitializeComponent()
-		TextBox1 = New TextBox()
-		PictureBox1 = New PictureBox()
-		CType(PictureBox1, ComponentModel.ISupportInitialize).BeginInit()
+		richData = New RichTextBox()
+		btnSend = New Button()
+		btnChangeInfo = New Button()
 		SuspendLayout()
 		' 
-		' TextBox1
+		' richData
 		' 
-		TextBox1.Location = New Point(101, 12)
-		TextBox1.Multiline = True
-		TextBox1.Name = "TextBox1"
-		TextBox1.Size = New Size(607, 129)
-		TextBox1.TabIndex = 0
+		richData.Location = New Point(12, 12)
+		richData.Name = "richData"
+		richData.Size = New Size(491, 272)
+		richData.TabIndex = 0
+		richData.Text = ""
 		' 
-		' PictureBox1
+		' btnSend
 		' 
-		PictureBox1.Location = New Point(81, 161)
-		PictureBox1.Name = "PictureBox1"
-		PictureBox1.Size = New Size(639, 277)
-		PictureBox1.TabIndex = 1
-		PictureBox1.TabStop = False
+		btnSend.Location = New Point(130, 300)
+		btnSend.Name = "btnSend"
+		btnSend.Size = New Size(75, 23)
+		btnSend.TabIndex = 1
+		btnSend.Text = "Gửi"
+		btnSend.UseVisualStyleBackColor = True
+		' 
+		' btnChangeInfo
+		' 
+		btnChangeInfo.Location = New Point(261, 300)
+		btnChangeInfo.Name = "btnChangeInfo"
+		btnChangeInfo.Size = New Size(139, 23)
+		btnChangeInfo.TabIndex = 2
+		btnChangeInfo.Text = "Thay đổi thông tin"
+		btnChangeInfo.UseVisualStyleBackColor = True
 		' 
 		' MainWindow
 		' 
 		AutoScaleDimensions = New SizeF(7F, 15F)
 		AutoScaleMode = AutoScaleMode.Font
-		ClientSize = New Size(800, 450)
-		Controls.Add(PictureBox1)
-		Controls.Add(TextBox1)
+		ClientSize = New Size(515, 335)
+		Controls.Add(btnChangeInfo)
+		Controls.Add(btnSend)
+		Controls.Add(richData)
 		Name = "MainWindow"
 		Text = "ClipSync"
-		CType(PictureBox1, ComponentModel.ISupportInitialize).EndInit()
 		ResumeLayout(False)
-		PerformLayout()
 	End Sub
-
-	Friend WithEvents TextBox1 As TextBox
-	Friend WithEvents PictureBox1 As PictureBox
 	Friend WithEvents notiIcon As NotifyIconManager
+	Friend WithEvents richData As RichTextBox
+	Friend WithEvents btnSend As Button
+	Friend WithEvents btnChangeInfo As Button
 
 End Class
--- WindowsClipSync/ClipSync/MainWindow.vb
+++ WindowsClipSync/ClipSync/MainWindow.vb
@@ -2,41 +2,51 @@
 Imports System.Windows.Forms
 Imports System.Windows.Forms.VisualStyles.VisualStyleElement
 Public Class MainWindow
-    ' Windows API
-    Private Const WM_CLIPBOARDUPDATE As Integer = &H31D
-    Private Shared ReadOnly HWND_MESSAGE As IntPtr = New IntPtr(-3)
+	' Windows API
+	Private Const WM_CLIPBOARDUPDATE As Integer = &H31D
+	Private Shared ReadOnly HWND_MESSAGE As IntPtr = New IntPtr(-3)
 
-    ' Import các hàm API
-    <DllImport("user32.dll", SetLastError:=True)>
-    Public Shared Function AddClipboardFormatListener(hWnd As IntPtr) As Boolean
-    End Function
-    <DllImport("user32.dll", SetLastError:=True)>
-    Public Shared Function RemoveClipboardFormatListener(hWnd As IntPtr) As Boolean
-    End Function
-    Protected Overrides Sub WndProc(ByRef m As Message)
-        MyBase.WndProc(m)
-        If m.Msg = WM_CLIPBOARDUPDATE Then
-            ' Clipboard đã thay đổi, xử lý nội dung mới
-            HandleClipboardChange()
-        End If
-    End Sub
-    Private Sub HandleClipboardChange()
-        If Clipboard.ContainsText() Then
-            Dim clipboardText As String = Clipboard.GetText()
-            TextBox1.Text = clipboardText  ' Hiển thị nội dung clipboard lên TextBox
-        ElseIf Clipboard.ContainsImage() Then
-            PictureBox1.Image = Clipboard.GetImage()  ' Hiển thị ảnh từ clipboard
-        End If
-    End Sub
+	' Import các hàm API
+	<DllImport("user32.dll", SetLastError:=True)>
+	Public Shared Function AddClipboardFormatListener(hWnd As IntPtr) As Boolean
+	End Function
 
-    Private Sub MainWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim notiIcon As NotifyIconManager = New NotifyIconManager(Me)
+	<DllImport("user32.dll", SetLastError:=True)>
+	Public Shared Function RemoveClipboardFormatListener(hWnd As IntPtr) As Boolean
+	End Function
 
-        ' Bắt đầu lắng nghe sự kiện clipboard
-        AddClipboardFormatListener(Me.Handle)
-    End Sub
+	Protected Overrides Sub WndProc(ByRef m As Message)
+		MyBase.WndProc(m)
+		If m.Msg = WM_CLIPBOARDUPDATE Then
+			' Clipboard changed
+			HandleClipboardChange()
+		End If
+	End Sub
 
-    Private Sub MainWindow_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+	Private Sub HandleClipboardChange()
+		If Clipboard.ContainsText() Then
+			Dim clipboardText As String = Clipboard.GetText()
 
-    End Sub
+			' TEXT DATA
+			' DO SOMETHING HERE
+			MsgBox(clipboardText)
+
+		ElseIf Clipboard.ContainsImage() Then
+			' IMAGE DATA
+			' DO SOMETHING HERE
+		End If
+	End Sub
+
+	Private Sub MainWindow_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+		Dim notiIcon As NotifyIconManager = New NotifyIconManager(Me)
+
+		AddClipboardFormatListener(Me.Handle)
+	End Sub
+
+	Private Sub btnChangeInfo_Click(sender As Object, e As EventArgs) Handles btnChangeInfo.Click
+		Dim infoChange As New InfoChange(Me)
+
+		infoChange.Show()
+		Me.Hide()
+	End Sub
 End Class
