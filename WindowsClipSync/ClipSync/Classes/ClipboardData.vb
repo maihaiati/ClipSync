ab6824915c0bbc423c81f78d0b18c87fe5e0fbf9
M2Tech <maiquanghai011@gmail.com>
2025-04-19 22:35:46 +0700

    Add new classes

---
+++ WindowsClipSync/ClipSync/Classes/ClipboardData.vb
@@ -0,0 +1,16 @@
+﻿Public Class ClipboardData
+	Private Shared latestData As String = ""
+
+	Public Shared Function getLatestData() As String
+		Return latestData
+	End Function
+
+	Public Shared Sub setLatestData(data As String)
+		latestData = data
+	End Sub
+
+	Public Shared Sub writeClipboard(text As String)
+		setLatestData(text)
+		Clipboard.SetText(text)
+	End Sub
+End Class
---
+++ WindowsClipSync/ClipSync/Classes/LocalIP.vb
@@ -0,0 +1,20 @@
+﻿Imports System.Net
+Imports System.Net.Sockets
+
+Public Class LocalIP
+	Public Shared Function getIPAddress() As String
+		Dim localIP As String = String.Empty
+
+		Try
+			Using socket As New Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp)
+				socket.Connect("8.8.8.8", 65530)
+				Dim endPoint As IPEndPoint = CType(socket.LocalEndPoint, IPEndPoint)
+				localIP = endPoint.Address.ToString()
+			End Using
+		Catch ex As Exception
+			ClipSyncDebug.log("[LocalIP] Lỗi lấy địa chỉ IP: " & ex.Message)
+		End Try
+
+		Return localIP
+	End Function
+End Class
